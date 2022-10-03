d9f885f01828063aecd7bfeae1a4bba3f000744a
Pavel Romanov <3metil@mail.ru>
2022-10-03 12:42:07 +0300

    Урок 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import './Dialogs.scss'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className='dialogs'>
+            <div className="dialogs-items">
+                <div className='dialog'>
+                    Dimych
+                </div>
+                <div className='dialog'>
+                    Adnrey
+                </div>
+                <div className='dialog'>
+                    Sveta
+                </div>
+                <div className='dialog'>
+                    Sasha
+                </div>
+                <div className='dialog'>
+                    Viktor
+                </div>
+                <div className='dialog'>
+                    Valera
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hi</div>
+                <div className="message">How are you?</div>
+                <div className="message">Yo</div>
+            </div>
         </div>
     )
 }
